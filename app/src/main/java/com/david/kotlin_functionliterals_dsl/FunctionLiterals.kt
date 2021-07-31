cea66b20b7b9eedafd59107fb9296beff51f65bd
David Guedez <david.guedez@gmail.com>
2021-07-31 17:55:00 -0400

    Added extension function as function parameter

--- app/src/main/java/com/david/kotlin_functionliterals_dsl/FunctionLiterals.kt
+++ app/src/main/java/com/david/kotlin_functionliterals_dsl/FunctionLiterals.kt
@@ -5,8 +5,63 @@ package com.david.kotlin_functionliterals_dsl
 */
 
 
+//1. classic function declaration
+fun appendTagChars(word: String): String {
+    return StringBuilder().append("<").append(word).append(">").toString()
+}
+
+//2. Extension function
+fun String.appendTag():String {
+   return StringBuilder().append("<").append(this).append(">").toString()
+}
+
+/*
+* 3. Lambdas are anonymous functions that can be treated as object been passed as function parameter, be returned by functions, value of variable, etc
+* */
+
+val square: (Int) -> Int = { value -> value * value }
+
+/*
+* 4. High order functions are functions that receives functions as parameters or returns them
+* */
+
+fun passMeAFunction(function: () -> Unit) {
+    //do something
+    println("I called passMeAFunction")
+    //call function whe it is needed
+    function()
+}
+
+
+//4.1 return function from another function
+fun add(a: Int, b: Int): Int = a + b
+
+fun returnMeAddFunction(): ((Int, Int) -> Int) {
+    return ::add
+}
+
+//4.2 high order function with parameters
+fun passArithmeticOperator(a: Int, b: Int, operator: ( Int, Int) -> Int): Int = operator(a, b)
+
+
+/*5
+* Extension functions applied to functions
+  Allows to use the properties of the receiver object using 'this' which is convenient when requiring lambdas as parameter
+ */
+
+ fun buildStringExtended(function: (StringBuilder).() -> Unit):String{
+     val builder = StringBuilder()
+     function(builder)
+     return builder.toString()
+ }
+
+
+
 fun main() {
-    //1. println(appendTag("David"))
+
+
+
+    //1. println(appendTagChars("David"))
 
     //2.
     val name = "David"
@@ -28,40 +83,22 @@ fun main() {
     passMeAFunction(innerFunction)
 
     //4.1
-  val add = returnMeAddFunction()
-    println("sum of 2 and 3 is equal to ${add(2,3)}")
+    val add = returnMeAddFunction()
+    println("sum of 2 and 3 is equal to ${add(2, 3)}")
+
+    //4.2
+
+    val addResult = passArithmeticOperator(7, 7, ::add)
+    println("Using add operator with 7 & 7 values return $addResult")
+
+    //5 Constructing lambda with a receiver
+    val newString = buildStringExtended {
+        append("<")
+        append("appending some text from extended function")
+        append(">")
+    }
+
+    println("Using external function result is $newString")
 }
 
-//1. classic function declaration
-fun appendTag(word: String): String {
-    return StringBuilder().append("<").append(word).append(">").toString()
-}
 
-//2. Extension function
-fun String.appendTag() {
-    StringBuilder().append("<").append(this).append(">").toString()
-}
-
-/*
-* 3. Lambdas are anonymous functions that can be treated as object been passed as function parameter, be returned by functions, value of variable, etc
-* */
-
-val square: (Int) -> Int = { value -> value * value }
-
-/*
-* 4. High order functions are functions that receives functions as parameters or returns them
-* */
-
-fun passMeAFunction(function: () -> Unit) {
-    //do something
-    println("I called passMeAFunction")
-    //call function whe it is needed
-    function()
-}
-
-//4.1 return function from another function
-fun add(a: Int, b: Int): Int = a + b
-
-fun returnMeAddFunction(): ((Int, Int) -> Int) {
-    return ::add
-}
